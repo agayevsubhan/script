596fd4a83e03017ce837fd01aa1771e14e5efeab
agayevsubhan <7nb9x7q@code.edu.az>
2023-04-12 00:13:40 +0400

    fghj

---
+++ app.js
@@ -0,0 +1,46 @@
+"use strict" 
+
+//cut ededler 
+
+// const arr=[7,55,32,91,87,21,74,252,2,-6,42,15,-41];
+// for (let i=0; i<arr.length; i++){
+//     if(arr[i] % 2 == 0){
+//         console.log(arr[i]);
+//     }
+// }
+
+// tek ededler menfini cixarma
+
+
+// for (let i=0; i<arr.length; i++){
+//     if(arr[i] % 2 == 1){
+//         console.log(arr[i]);
+//     }
+// }
+
+//menfi olanlari ekrana cixarin
+
+
+// for (let i=0; i<arr.length; i++){
+//     if(arr[i] % 2 == -1 ||arr[i] % 4 == -2  ){
+//         console.log(arr[i]);
+//     }
+// }
+
+// her bir ededin iki qatini ekrana cixarin 
+// const arr=[7,55,32,91,87,21,74,252,2,-6,42,15,-41];
+// for (let i=0; i<arr.length; i++){
+//     if(arr[i] *=2  ){
+//         console.log(arr[i]);
+//     }
+// }
+
+//40  dan boyuk olanlari ekrana cixarin
+
+
+// for (let i=0; i<arr.length; i++){
+//     if(arr[i] >40  ){
+//         console.log(arr[i]);
+//     }
+// }
+
